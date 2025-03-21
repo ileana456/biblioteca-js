7a9e784231966da94debc66ab07d8d3196c3f452
ileana <est.ijalfarob0201@uml.edu.ni>
2025-03-21 15:34:43 -0600

    Añadir subclase Ebook

--- Libro.js
+++ Libro.js
@@ -8,3 +8,13 @@ class Libro {
   
   // Exporta la clase
   module.exports = Libro;
+  class Ebook extends Libro {
+    constructor(titulo, autor, añoPublicacion, precio, formato) {
+    }
+    
+    mostrarDetalles() {
+  
+    }
+  }
+  
+  // Añade la exportación de la clase Ebook
